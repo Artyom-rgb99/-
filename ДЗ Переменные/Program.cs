d079d197c47b395e24b30a2327433f4f89774d69
Admin <Admin@ARTYOM>
2024-11-11 15:10:13 +0700

    Добавьте файлы проекта.

---
+++ ДЗ Переменные/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ДЗ_Переменные
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte people = 30;
+
+            int money = 23;
+
+            uint time = 60;
+
+            byte pig = 40;
+
+            ushort places = 60;
+
+            char symbol = 'T';
+
+            float dogPlay = 7.3F;
+
+            double dogPlayOne = 8.3;
+
+            bool boarding;
+            boarding = people >= places;
+
+            string greeting = "Welcome to our cafe";
+
+
+
+        }
+    }
+}
